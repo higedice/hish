7972936b6e09e003f5a7eca1b92a07f1b6520488
HigeDice <higedice@banjoyugi.net>
2018-06-22 01:01:34 +0900

    devided one source file into multiple files.

---
+++ src/builtin_commands.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "builtin_commands.h"
+
+static int hish_num_builtins(void);
+static int hish_cd(char **args);
+static int hish_help(char **args);
+static int hish_exit(char **args);
+
+static char *builtin_str[] = {
+	"cd",
+	"help",
+	"exit"
+};
+
+
+static int (*builtin_func[]) (char **) = {
+	&hish_cd,
+	&hish_help,
+	&hish_exit
+};
+
+
+int exec_if_builtin_cmd(char *cmd, char **args)
+{
+	int i;
+
+	for (i = 0; i < hish_num_builtins(); i++) {
+		if (strcmp(args[0], builtin_str[i]) == 0) {
+			return (*builtin_func[i])(args);
+		}
+	}
+
+	return -1;
+}
+
+
+static int hish_num_builtins()
+{
+	return sizeof(builtin_str) / sizeof(char *);
+}
+
+static int hish_cd(char **args)
+{
+	if (args[1] == NULL) {
+		fprintf(stderr, "hish: expected argument to \"cd\"\n");
+	} else {
+		if (chdir(args[1]) != 0) {
+			perror("hish");
+		}
+	}
+
+	return 1;
+}
+
+
+static int hish_help(char **args)
+{
+	int i;
+	printf("Stephen Brennan's hish\n");
+	printf("Type Program names and arguments, and hit enter.\n");
+	printf("The following are built in:\n");
+
+	for (i = 0; i< hish_num_builtins(); i++) {
+		printf("  %s\n", builtin_str[i]);
+	}
+
+	printf("Use the man command for information on other programs.\n");
+
+	return 1;
+}
+
+
+static int hish_exit(char **args)
+{
+	return 0;
+}
+
---
+++ src/builtin_commands.h
@@ -0,0 +1,1 @@
+int exec_if_builtin_cmd(char *cmd, char **args);
