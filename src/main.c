f3a550a53645256016206f9fa0bc3a70082b0ac1
HigeDice <higedice@banjoyugi.net>
2018-06-24 14:31:30 +0900

    make functions to static and refactoring.

--- src/main.c
+++ src/main.c
@@ -8,53 +8,115 @@
 #include "prompt.h"
 
 
-int hish_launch(char **args)
-{
-	pid_t pid;
-	int status;
+static void hish_loop(FILE *fpin);
+static char **hish_split_line(char *line);
+static char *hish_read_line(FILE *fpin);
+static int hish_execute(char **args);
+static int hish_launch(char **args);
 
-	pid = fork();
-	if (pid == 0) {
-		// Child process
-		if (execvp(args[0], args) == -1) {
-			perror("hish");
-		}
-		exit(EXIT_FAILURE);
-	} else if (pid < 0) {
-		// Error forking
-		perror("hish");
+
+
+int main(int argc, char **argv)
+{
+	FILE *fp;
+
+	// load config files, if any.
+
+
+	if (argc == 1) {
+		// Run command loop.
+		hish_loop(stdin);
+	} else if (argc > 2) {
+		fprintf(stderr, "hish: too match arguments.\n");
 	} else {
-		// Parent process
-		do {
-			waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		fp = fopen(argv[1], "r");
+		if (fp == NULL) {
+			fprintf(stderr, "hish: can't open %s.\n", argv[1]);
+			exit(EXIT_FAILURE);
+		} else {
+			// Run command loop.
+			hish_loop(fp);
+			fclose(fp);
+		}
+	}
+	if (ferror(stdout)) {
+		fprintf(stderr, "hish: error writing stdout.\n");
+		exit(EXIT_FAILURE);
 	}
 
-	return 1;
+
+
+	// Perform any shutdown/cleanup.
+
+	return EXIT_SUCCESS;
 }
 
 
-int hish_execute(char **args)
+
+static void hish_loop(FILE *fpin)
 {
-	int i;
+	char *line;
+	char **args;
 	int status;
 
-	if (args[0] == NULL) {
-		// An empty command was entered.
-		return 1;
-	}
+	do {
+		if (fpin == stdin) {
+			print_prompt();
+		}
+		line = hish_read_line(fpin);
+		args = hish_split_line(line);
+		status = hish_execute(args);
 
-	if ((status = exec_if_builtin_cmd(args[0], args)) >= 0) {
-		return status;
-	}
-
-	return hish_launch(args);
+		free(line);
+		free(args);
+	} while (status);
 }
+
+
+
+#define HISH_TOK_BUFSIZE 64
+#define HISH_TOK_DELIM " \t\r\n\a"
+
+static char **hish_split_line(char *line)
+{
+	int bufsize = HISH_TOK_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *token, **tokens_backup;
+
+	if (!tokens) {
+		fprintf(stderr, "hish: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, HISH_TOK_DELIM);
+	while (token != NULL) {
+		tokens[position] = token;
+		position++;
+
+		if (position >= bufsize) {
+			bufsize += HISH_TOK_BUFSIZE;
+			tokens_backup = tokens;
+			tokens = realloc(tokens, bufsize * sizeof(char*));
+			if (!tokens) {
+				free(tokens_backup);
+				fprintf(stderr, "hish: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		token = strtok(NULL, HISH_TOK_DELIM);
+	}
+
+	tokens[position] = NULL;
+
+	return tokens;
+}
+
 
 
 #define HISH_RL_BUFSIZE 1024
 
-char *hish_read_line(FILE *fpin)
+static char *hish_read_line(FILE *fpin)
 {
 	int bufsize = HISH_RL_BUFSIZE;
 	int position = 0;
@@ -97,103 +159,50 @@ char *hish_read_line(FILE *fpin)
 }
 
 
-#define HISH_TOK_BUFSIZE 64
-#define HISH_TOK_DELIM " \t\r\n\a"
 
-char **hish_split_line(char *line)
+
+static int hish_execute(char **args)
 {
-	int bufsize = HISH_TOK_BUFSIZE, position = 0;
-	char **tokens = malloc(bufsize * sizeof(char*));
-	char *token, **tokens_backup;
-
-	if (!tokens) {
-		fprintf(stderr, "hish: allocation error\n");
-		exit(EXIT_FAILURE);
-	}
-
-	token = strtok(line, HISH_TOK_DELIM);
-	while (token != NULL) {
-		tokens[position] = token;
-		position++;
-
-		if (position >= bufsize) {
-			bufsize += HISH_TOK_BUFSIZE;
-			tokens_backup = tokens;
-			tokens = realloc(tokens, bufsize * sizeof(char*));
-			if (!tokens) {
-				free(tokens_backup);
-				fprintf(stderr, "hish: allocation error\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-
-		token = strtok(NULL, HISH_TOK_DELIM);
-	}
-
-	tokens[position] = NULL;
-
-	return tokens;
-}
-
-
-void hish_loop(FILE *fpin)
-{
-	char *line;
-	char **args;
+	int i;
 	int status;
 
-	do {
-		if (fpin == stdin) {
-			print_prompt();
-		}
-		line = hish_read_line(fpin);
-		args = hish_split_line(line);
-		status = hish_execute(args);
+	if (args[0] == NULL) {
+		// An empty command was entered.
+		return 1;
+	}
 
-		free(line);
-		free(args);
-	} while (status);
+	if ((status = exec_if_builtin_cmd(args[0], args)) >= 0) {
+		return status;
+	}
+
+	return hish_launch(args);
 }
 
 
-int main(int argc, char **argv)
+
+static int hish_launch(char **args)
 {
-	FILE *fp;
+	pid_t pid;
+	int status;
 
-	// load config files, if any.
-
-
-	if (argc == 1) {
-		// Run command loop.
-		hish_loop(stdin);
-	} else if (argc > 2) {
-		fprintf(stderr, "hish: too match arguments.\n");
-	} else {
-		fp = fopen(argv[1], "r");
-		if (fp == NULL) {
-			fprintf(stderr, "hish: can't open %s.\n", argv[1]);
-			exit(EXIT_FAILURE);
-		} else {
-			// Run command loop.
-			hish_loop(fp);
-			fclose(fp);
+	pid = fork();
+	if (pid == 0) {
+		// Child process
+		if (execvp(args[0], args) == -1) {
+			perror("hish");
 		}
-	}
-	if (ferror(stdout)) {
-		fprintf(stderr, "hish: error writing stdout.\n");
 		exit(EXIT_FAILURE);
+	} else if (pid < 0) {
+		// Error forking
+		perror("hish");
+	} else {
+		// Parent process
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 
-
-
-	// Perform any shutdown/cleanup.
-
-	return EXIT_SUCCESS;
+	return 1;
 }
-
-
-
-
-
 
 
